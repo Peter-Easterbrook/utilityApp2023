aa159dfb57a2c44fee25f949eb0cbcdf699daf70
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-04-02 20:46:01 +0200

    Saves DM in localStorage

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,9 +1,17 @@
 const toggle = document.getElementById('toggle');
+const html = document.querySelector('html');
+const isDarkMode = localStorage.getItem('isDarkMode');
+
+if (isDarkMode === 'true') {
+  html.classList.add('dark');
+}
+
 toggle.addEventListener('click', (e) => {
-  const html = document.querySelector('html');
   if (html.classList.contains('dark')) {
     html.classList.remove('dark');
+    localStorage.setItem('isDarkMode', 'false');
   } else {
     html.classList.add('dark');
+    localStorage.setItem('isDarkMode', 'true');
   }
 });
