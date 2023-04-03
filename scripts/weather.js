c0fcd6fe81959c6584884c092dca82853f1ce190
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-04-03 08:54:01 +0200

    Tweaks DM code so that it functions automatically across pages

--- scripts/weather.js
+++ scripts/weather.js
@@ -100,12 +100,12 @@
   }
 })();
 
-const toggle = document.getElementById('toggle');
-toggle.addEventListener('click', (e) => {
-  const html = document.querySelector('html');
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark');
-  } else {
-    html.classList.add('dark');
-  }
-});
+// const toggle = document.getElementById('toggle');
+// toggle.addEventListener('click', (e) => {
+//   const html = document.querySelector('html');
+//   if (html.classList.contains('dark')) {
+//     html.classList.remove('dark');
+//   } else {
+//     html.classList.add('dark');
+//   }
+// });
