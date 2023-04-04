7d6d655a56357f918087ed78a710bd9afa3498ed
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-04-04 11:54:54 +0200

    Page alignment

--- scripts/clock.js
+++ scripts/clock.js
@@ -86,13 +86,3 @@ function updateTime() {
 
 // Call updateTime() every second
 setInterval(updateTime, 1000);
-
-// const toggle = document.getElementById('toggle');
-// toggle.addEventListener('click', (e) => {
-//   const html = document.querySelector('html');
-//   if (html.classList.contains('dark')) {
-//     html.classList.remove('dark');
-//   } else {
-//     html.classList.add('dark');
-//   }
-// });
--- scripts/weather.js
+++ scripts/weather.js
@@ -99,13 +99,3 @@
     return `${day} ${date} ${month} ${year} `;
   }
 })();
-
-// const toggle = document.getElementById('toggle');
-// toggle.addEventListener('click', (e) => {
-//   const html = document.querySelector('html');
-//   if (html.classList.contains('dark')) {
-//     html.classList.remove('dark');
-//   } else {
-//     html.classList.add('dark');
-//   }
-// });
